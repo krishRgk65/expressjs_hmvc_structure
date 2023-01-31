9f5545bef80154a4bddfdca96c8a097c1764c00b
krishRgk65 <124018659+krishRgk65@users.noreply.github.com>
2023-01-31 09:35:52 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+  app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+require("./app/config/config.router.js")(app);
+// set port, listen for requests
+const PORT =  3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
